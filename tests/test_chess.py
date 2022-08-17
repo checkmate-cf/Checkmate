b3ce78f44f79fc4535206d55f7771df74a4ea7ee
Benjamin Small <smallbenjamin94@gmail.com>
2022-08-17 10:14:52 -0700

    added test for black check and checkmate

--- tests/test_chess.py
+++ tests/test_chess.py
@@ -113,3 +113,67 @@ def test_move_black_pawn_two_not_home():
     actual = c.validate_move("A7", "A5", board, "black")
     expected = False
     assert actual == expected
+
+
+# @pytest.mark.skip("pending")
+def test_black_king_not_check(standard_board):
+    actual = c.check_check("black", standard_board)
+    expected = False
+    assert actual == expected
+
+
+# @pytest.mark.skip("pending")
+def test_black_king_check(black_in_check_not_checkmate):
+    actual = c.check_check("black", black_in_check_not_checkmate)
+    expected = True
+    assert actual == expected
+
+
+# @pytest.mark.skip("pending")
+def test_black_king_check_not_checkmate(black_in_check_not_checkmate):
+    actual = c.check_checkmate("black", black_in_check_not_checkmate)
+    expected = False
+    assert actual == expected
+
+
+# @pytest.mark.skip("pending")
+def test_black_king_checkmate(black_in_checkmate):
+    actual = c.check_checkmate("black", black_in_checkmate)
+    expected = True
+    assert actual == expected
+
+@pytest.fixture
+def standard_board():
+    standard_board = b.Chessboard([["{r}", "{k}", "{b}", "{Q}", "{K}", "{b}", "{k}", "{r}"],
+                                   ["{p}", "{p}", "{p}", "{p}", "{p}", "{p}", "{p}", "{p}"],
+                                   ["x",    "x",   "x",   "x",   "x",   "x",   "x",   "x"],
+                                   ["x",    "x",   "x",   "x",   "x",   "x",   "x",   "x"],
+                                   ["x",    "x",   "x",   "x",   "x",   "x",   "x",   "x"],
+                                   ["x",    "x",   "x",   "x",   "x",   "x",   "x",   "x"],
+                                   ["[p]", "[p]", "[p]", "[p]", "[p]", "[p]", "[p]", "[p]"],
+                                   ["[r]", "[k]", "[b]", "[Q]", "[K]", "[b]", "[k]", "[r]"]])
+    return standard_board
+
+@pytest.fixture
+def black_in_check_not_checkmate():
+    black_in_check_not_checkmate = b.Chessboard([["{r}", "{k}", "{b}", "{Q}", "{K}", "{b}", "{k}", "{r}"],
+                                                 ["x",  "{p}", "{p}", "{p}", "{p}", "[Q]", "{p}", "{p}"],
+                                                 ["x",    "x",   "x",   "x",   "x",   "x",   "x",   "x"],
+                                                 ["x",    "x",   "x",   "x",   "x",   "x",   "x",   "x"],
+                                                 ["{p}",  "x",   "x",   "x",   "x",   "x",   "x",   "x"],
+                                                 ["x",    "x",   "x",   "x",  "[p]",  "x",   "x",   "x"],
+                                                 ["[p]", "[p]", "[p]", "[p]",   "x", "[p]", "[p]", "[p]"],
+                                                 ["[r]", "[k]", "[b]",  "x", "[K]", "[b]", "[k]", "[r]"]])
+    return black_in_check_not_checkmate
+
+@pytest.fixture
+def black_in_checkmate():
+    black_in_check_not_checkmate = b.Chessboard([["{r}", "{k}", "{b}", "{Q}", "{K}", "{b}", "{k}", "{r}"],
+                                                 ["x",   "{p}", "{p}", "{p}", "{p}", "[Q]", "{p}", "{p}"],
+                                                 ["x",    "x",   "x",   "x",   "x",   "x",   "x",   "x"],
+                                                 ["x",    "x",   "x",   "x",   "x",   "x",   "x",   "x"],
+                                                 ["{p}",  "x",  "[b]",  "x",   "x",   "x",   "x",   "x"],
+                                                 ["x",    "x",   "x",   "x",  "[p]",  "x",   "x",   "x"],
+                                                 ["[p]", "[p]", "[p]", "[p]",  "x",  "[p]", "[p]", "[p]"],
+                                                 ["[r]", "[k]", "[b]",  "x", "[K]",  "x", "[k]", "[r]"]])
+    return black_in_check_not_checkmate
